c593fdc1d3d1e09009bef9a6b268408b09f25517
Baran Adalı <baaranadali@hotmail.com>
2024-11-26 02:04:55 +0300

    Update Vercel Analytics.

--- app/layout.js
+++ app/layout.js
@@ -45,6 +45,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistMono.variable} antialiased bg-black`}>
+        <Analytics/>
         {children}
       </body>
     </html>
